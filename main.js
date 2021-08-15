f3db1d142ec1443df017952d123badde31b717a4
benmez1n <ghomri.imad1@gmail.com>
2021-08-15 11:08:27 +0100

    Add the pagination list

--- main.js
+++ main.js
@@ -1,15 +1,47 @@
 const items = Array.from(document.querySelectorAll('#slider-container img')),
     prvBtn = document.getElementById("previous"),
     nextBtn = document.getElementById("next");
-let currentItem = 0;
+    
+let currentItem = 0,
+    listItems=[],
+    paginationList = document.createElement("ul");
+
+paginationList.setAttribute("id","pagination-list");
+document.getElementById("pagination-container").appendChild(paginationList);
+
+for (let i=0;i<items.length;i++){
+    listItems[i] = document.createElement('li');
+    paginationList.appendChild(listItems[i]);
+    listItems[i].textContent = `${i+1}`;
+    if(i==0)listItems[i].classList.add("active");
+}
+
+listItems.forEach((element,index)=>{
+    element.onclick = () => {
+        remover();
+        active(index);
+        currentItem=index;
+    }
+});
+
 nextBtn.onclick = ()=>{
-    if(currentItem==items.length)currentItem=0
+    if(currentItem==items.length-1)currentItem=-1;
     remover();
-    items[currentItem++].classList.add("active");
+    active(++currentItem);
 }
+
 prvBtn.onclick = ()=>{
-    if(currentItem==-1)currentItem=items.length-1
+    if(currentItem==0)currentItem=items.length
     remover();
-    items[currentItem--].classList.add("active");
+    active(--currentItem);
 }
-remover = ()=>{items.forEach(element=>element.classList.remove("active"))}
+
+remover = ()=>{
+    items.forEach(element=>element.classList.remove("active"))
+    listItems.forEach(element=>element.classList.remove("active"))
+}
+
+active = currentItem =>{
+    items[currentItem].classList.add("active");
+    listItems[currentItem].classList.add("active");
+}
